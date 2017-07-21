835931ee9fc682737d68f7faab209cc43aaa556b
hiimbex <hiimbex@github.com>
2017-07-21 14:58:47 -0700

    beginnings of auto-labeler

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const OwnerNotifier = require('./lib/owner-notifier');
+const WatcherNotifier = require('./lib/watcher-notifier');
+
 
 module.exports = robot => {
-  const notify = async function (event) {
+  const notifyOwners = async function (event) {
     const github = await robot.auth(event.payload.installation.id);
     const notifier = new OwnerNotifier(github, event);
 
     return notifier.check();
   };
 
-  robot.on('pull_request.opened', notify);
-  robot.on('pull_request.synchronize', notify);
+  const notifyWatchers = async function (event) {
+    const github = await robot.auth(event.payload.installation.id);
+    const notifier = new WatcherNotifier(github, event);
+
+    return notifier.check();
+  };
+
+  robot.on('pull_request.opened', notifyOwners);
+  robot.on('pull_request.synchronize', notifyOwners);
+  robot.on('pull_request.labeled', notifyWatchers);
 };
